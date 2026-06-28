5d33a2a432a2278a70068a6570e3332295d2999e
Cyril Rozario <cyrilrozario7@gmail.com>
2026-06-28 06:55:13 +0530

    private, static and final scanner.

--- src/UserPrompt.java
+++ src/UserPrompt.java
@@ -2,13 +2,9 @@ import java.util.Scanner;
 
 public class UserPrompt {
 
-    private static Scanner sc;
+    private static final Scanner sc = new Scanner(System.in);
 
-    public static Scanner getInstance() {
-
-        if(sc == null) {
-            sc = new Scanner(System.in);
-        }
-        return sc;
+    public static int userInput() {
+        return sc.nextInt();
     }
 }
