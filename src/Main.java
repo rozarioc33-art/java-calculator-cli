780fe2dceb9f0e0cc6cf4411d0e0c9426b10b53f
Cyril Rozario <cyrilrozario7@gmail.com>
2026-06-29 08:08:43 +0530

    inner and outer loops

--- src/Main.java
+++ src/Main.java
@@ -3,52 +3,58 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        boolean repeat = false;
 
-        System.out.println("Enter two numbers: ");
-        int a = UserPrompt.userInput();
-        int b = UserPrompt.userInput();
+        int choice;
+        boolean chk;
+        boolean repeat = true;
+
 
         Calculator cal = new Calculator();
 
-        int choice;
-        boolean chk = false;
+        while (repeat) {
 
-        do {
-            System.out.println("Choose any operation: \n 1. Addition \n 2. Substraction \n 3. Multiplication \n 4. Division");
-            choice = UserPrompt.userInput();
+            System.out.println("Enter two numbers: ");
+            int a = UserPrompt.userInput();
+            int b = UserPrompt.userInput();
 
-            switch(choice) {
-                case 1:
-                    System.out.println("Addition");
-                    int sum = cal.add(a, b);
-                    System.out.println("Sum: " +sum);
-                    break;
-                case 2:
-                    System.out.println("Substraction");
-                    int sub = cal.sub(a, b);
-                    System.out.println("Difference: " +sub);
-                    break;
-                case 3:
-                    System.out.println("Multiplication");
-                    int mul = cal.multiply(a, b);
-                    System.out.println("Multiplication: " +mul);
-                    break;
-                case 4:
-                    System.out.println("Division");
-                    int div = cal.divide(a, b);
-                    System.out.println("Division: " +div);
-                    break;
-                default:
-                    System.out.println("Enter a valid choice....!");
-                    chk = true;
-            }
+            do {
+                chk = false;
+                System.out.println("Choose any operation: \n 1. Addition \n 2. Substraction \n 3. Multiplication \n 4. Division");
+                choice = UserPrompt.userInput();
 
-            if (!chk) {
-                repeat = cal.repeatPrompt();
-            }
+                switch(choice) {
+                    case 1:
+                        System.out.println("Addition");
+                        int sum = cal.add(a, b);
+                        System.out.println("Sum: " +sum);
+                        break;
+                    case 2:
+                        System.out.println("Substraction");
+                        int sub = cal.sub(a, b);
+                        System.out.println("Difference: " +sub);
+                        break;
+                    case 3:
+                        System.out.println("Multiplication");
+                        int mul = cal.multiply(a, b);
+                        System.out.println("Multiplication: " +mul);
+                        break;
+                    case 4:
+                        System.out.println("Division");
+                        int div = cal.divide(a, b);
+                        System.out.println("Division: " +div);
+                        break;
+                    default:
+                        System.out.println("Enter a valid choice....!");
+                        chk = true;
+                }
 
-        } while(chk || repeat);
+                if (!chk) {
+                    repeat = cal.repeatPrompt();
+                }
+
+            } while(chk);
+
+        }
 
         System.out.println("Exiting...");
         UserPrompt.close();
